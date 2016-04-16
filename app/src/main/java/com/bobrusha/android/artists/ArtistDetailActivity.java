b7bc26848bd90f7376369f7f7a42e79fd5595b09
bobrusha <bobrova.aleksand@gmail.com>
2016-04-17 01:40:46 +0300

    Refactoring.

--- app/src/main/java/com/bobrusha/android/artists/ArtistDetailActivity.java
+++ app/src/main/java/com/bobrusha/android/artists/ArtistDetailActivity.java
@@ -13,7 +13,7 @@ public class ArtistDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_artist_info);
+        setContentView(R.layout.activity_artist_detail);
     }
 
     @Override
