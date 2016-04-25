06438120a59582b33081f703942ecad632d65c04
bobrusha <bobrova.aleksand@gmail.com>
2016-04-25 15:54:41 +0300

    Added title for main activity.

--- app/src/main/java/com/bobrusha/android/artists/MainActivity.java
+++ app/src/main/java/com/bobrusha/android/artists/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_artists);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
