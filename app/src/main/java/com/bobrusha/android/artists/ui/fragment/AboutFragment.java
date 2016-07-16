73ad9d6b5a482ee558a89e547523f83086486d56
Aleksandra Bobrova <bobrova.aleksand@gmail.com>
2016-07-17 02:51:00 +0300

    Added drawer layout and fragment (about).

---
+++ app/src/main/java/com/bobrusha/android/artists/ui/fragment/AboutFragment.java
@@ -0,0 +1,10 @@
+package com.bobrusha.android.artists.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Aleksandra on 16/07/16.
+ */
+public class AboutFragment extends Fragment {
+
+}
